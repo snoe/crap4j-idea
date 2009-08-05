85d1d9b1c8a95f340e2dfa85454ac1fad374894f
snoe <snoe@snoe.ca>
2009-08-05 11:43:55 -0600

    Added multi file crap check

--- src/com/intellij/plugin/crap4j/ScanChanges.java
+++ src/com/intellij/plugin/crap4j/ScanChanges.java
@@ -5,17 +5,43 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.PsiManager;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public class ScanFileAction extends AnAction {
+public class ScanChanges extends AnAction {
     public void actionPerformed(AnActionEvent event) {
-	try {
 
-	    PsiJavaFile psiFile = (PsiJavaFile) DataKeys.PSI_FILE.getData(event.getDataContext());
+	try {
 	    Project project = DataKeys.PROJECT.getData(event.getDataContext());
-	    Map<String, Double> crapMap = CrapChecker.crapCheckFile(psiFile, project);
+	    PsiElement[] elements = DataKeys.PSI_ELEMENT_ARRAY.getData(event.getDataContext());
+	    VirtualFile[] vfs = DataKeys.VIRTUAL_FILE_ARRAY.getData(event.getDataContext());
+	    Map<String, Double> crapMap = new HashMap<String, Double>();
+	    if (elements != null){
+		for (PsiElement element : elements) {
+		    PsiFile file = element.getContainingFile();
+		    if (file instanceof PsiJavaFile) {
+			PsiJavaFile psiFile = (PsiJavaFile) file;
+			crapMap.putAll(CrapChecker.crapCheckFile(psiFile, project));
+		    }
+		}
+	    }
+
+	    if (vfs != null){
+		for (VirtualFile vf : vfs) {
+		    PsiFile file = PsiManager.getInstance(project).findFile(vf);
+		    if (file instanceof PsiJavaFile) {
+			PsiJavaFile psiFile = (PsiJavaFile) file;
+			crapMap.putAll(CrapChecker.crapCheckFile(psiFile, project));
+		    }
+		}
+	    }
+
 	    StringBuilder crapStr = new StringBuilder();
 	    for (Map.Entry<String, Double> entry : crapMap.entrySet()) {
 		crapStr.append("CRAP: " + entry.getValue() + " METHOD: " + entry.getKey() + "\n\n");
@@ -31,5 +57,4 @@ public class ScanFileAction extends AnAction {
 	    throw new RuntimeException(e);
 	}
     }
-
 }
